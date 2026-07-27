a76efa6097642583258ed0baf10d8d829e6b3ce1
Fernando sanchez <fernandosanchez2003636@gmail.com>
2026-07-26 20:22:15 -0500

    Configure Firebase Authentication

---
+++ rutapro-web/src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAK8RZkdLkA6ilU9d-0JFG8C_Rq_KSWwUo",
+  authDomain: "rutapro-b4f9c.firebaseapp.com",
+  projectId: "rutapro-b4f9c",
+  storageBucket: "rutapro-b4f9c.firebasestorage.app",
+  messagingSenderId: "166677189261",
+  appId: "1:166677189261:web:7547b3351cfa141dbf48c4",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
